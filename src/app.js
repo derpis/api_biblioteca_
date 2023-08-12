0f33194a1961b5b5451bad19655aea05479f3d48
derpis <santicarr2005@gmail.com>
2023-08-12 14:59:04 -0300

    issuerBaseURL: "https://dev-utn-frc-iaew.auth0.com/",

--- src/app.js
+++ src/app.js
@@ -8,8 +8,8 @@ require('dotenv').config();
 
 // Configuracion Middleware con el Servidor de Autorización 
 const autenticacion = auth({
-  audience: process.env.OAUTH_AUDIENCE,
-  issuerBaseURL: process.env.OAUTH_URL,
+  audience: "http://localhost:3000/api/biblioteca",
+  issuerBaseURL: "https://dev-utn-frc-iaew.auth0.com/",
   tokenSigningAlg: "RS256",
 });
 
@@ -22,6 +22,60 @@ const librosRouter = require("./routes/libros");
 
 //Configuramos el middleware de autenticacion
 app.use("/api/libros", autenticacion,  librosRouter);
+
+// Datos de ejemplo para simular una lista de usuarios
+const usuarios = [
+  { id: 1, nombre: 'Usuario 1' },
+  { id: 2, nombre: 'Usuario 2' },
+  { id: 3, nombre: 'Usuario 3' }
+];
+
+// Ruta GET para obtener la lista de usuarios
+app.get('/usuarios', (req, res) => {
+  res.json(usuarios);
+});
+
+// Ruta GET para obtener los detalles de un usuario específico según su ID
+app.get('/usuarios/:id', (req, res) => {
+  const idUsuario = parseInt(req.params.id);
+  const usuario = usuarios.find(user => user.id === idUsuario);
+
+  if (usuario) {
+    res.json(usuario);
+  } else {
+    res.status(404).json({ mensaje: 'Usuario no encontrado' });
+  }
+});
+
+// Ruta POST para crear un nuevo usuario
+app.post('/usuarios', (req, res) => {
+  const nuevoUsuario = req.body;
+  usuarios.push(nuevoUsuario);
+  res.status(201).json(nuevoUsuario);
+});
+
+// Ruta PUT para actualizar la información de un usuario según su ID
+app.put('/usuarios/:id', (req, res) => {
+  const idUsuario = parseInt(req.params.id);
+  const usuarioActualizado = req.body;
+
+  usuarios = usuarios.map(user => {
+    if (user.id === idUsuario) {
+      return { ...user, ...usuarioActualizado };
+    }
+    return user;
+  });
+
+  res.json(usuarioActualizado);
+});
+
+// Ruta DELETE para eliminar un usuario según su ID
+app.delete('/usuarios/:id', (req, res) => {
+  const idUsuario = parseInt(req.params.id);
+  usuarios = usuarios.filter(user => user.id !== idUsuario);
+  res.json({ mensaje: 'Usuario eliminado exitosamente' });
+});
+
 
 app.use(errorHandler);
 
--- src/models/libroModel.js
+++ src/models/libroModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_DB, {
+mongoose.connect("mongodb://localhost:3000", {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
